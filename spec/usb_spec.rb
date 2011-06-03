432898b547544ca7ca642cd329c733cbc0f81182
David Grayson <davidegrayson@gmail.com>
2011-06-02 23:00:08 -0700

    Improved tests

--- spec/usb_spec.rb
+++ spec/usb_spec.rb
@@ -7,13 +7,27 @@ describe Usb do
 
   it "can list devices" do
     devices = Usb.get_device_list
-    devices.size.should > 0
+    devices.size.should > 1
     devices.sort_by! { |device| [device.bus_number, device.address] }
     devices.each do |device|
-      device.class.should == Usb::Device
-      puts "#{device} #{device.bus_number} #{device.address}"
-      lambda { device.max_packet_size(0) }.should raise_error Usb::NotFoundError
-      lambda { device.max_packet_size(:foo) }.should raise_error TypeError
+      device.should be_a_kind_of Usb::Device
+      device.bus_number.should be_between 0, 20
+      device.address.should be_between 0, 127
     end
+  end
+end
+
+describe "Usb::Device#max_packet_size" do
+  before do
+    @device = Usb::get_device_list.first
+  end
+
+  it "makes sure its argument is an int" do
+    lambda { @device.max_packet_size(2**64) }.should raise_error RangeError
+    lambda { @device.max_packet_size(:foo) }.should raise_error TypeError
+  end
+
+  it "doesn't work" do
+    lambda { @device.max_packet_size(0) }.should raise_error Usb::NotFoundError
   end
 end
