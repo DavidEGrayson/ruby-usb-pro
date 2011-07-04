923f021e3d4dba5d2923964ee7d5ae92e8c8bba7
David Grayson <davidegrayson@gmail.com>
2011-07-04 00:28:54 -0700

    Restructured descriptors.rb

--- lib/ruby-usb-pro/descriptors.rb
+++ lib/ruby-usb-pro/descriptors.rb
@@ -8,84 +8,90 @@ class Usb::DeviceDescriptor < Struct.new(:bLength, :bDescriptorType,
 end
 
 module Usb::Descriptors
+  class Configuration
+    attr_accessor :wLength
+    attr_accessor :bNumInterfaces
+    attr_accessor :bConfigurationValue
+    attr_accessor :iConfiguration
+    attr_accessor :bmAttributes
+    attr_accessor :bMaxPower
 
-end
+    attr_accessor :descendents
+    attr_accessor :children
 
-class Usb::Descriptors::Configuration
-  attr_accessor :wLength
-  attr_accessor :bNumInterfaces
-  attr_accessor :bConfigurationValue
-  attr_accessor :iConfiguration
-  attr_accessor :bmAttributes
-  attr_accessor :bMaxPower
+    SubDescriptors = {}
 
-  attr_accessor :descendents
-  attr_accessor :children
-
-  SubDescriptors = {}
-
-  def initialize()
-    @descendents = []
-    @children = []
-  end
-
-  def self.next_descriptor_binary(binary)
-    raise ArgumentError if binary.empty?
-    bLength = binary[0].ord
-    raise Usb::DescriptorParsingError, "Unexpected end of descriptors: bLength=#{bLength} but only #{binary.length} bytes remaining." if binary.length < bLength 
-    descriptor = binary[0, bLength]
-    binary.replace(binary[bLength..-1])
-    return descriptor
-  end
-
-  def self.from_binary(binary)
-    config = new
-    bLength, bDescriptorType, wTotalLength, config.bNumInterfaces, config.bConfigurationValue, config.iConfiguration, config.bmAttributes, config.bMaxPower = binary.unpack('CCvCCCCC')
-
-    raise Usb::DescriptorParsingError, "Expected bLength of configuration descriptor to be 9, but got #{bLength}." if bLength != 9
-
-    binary = binary[9..-1]
-
-    while !binary.empty?
-      # Get the descriptor's binary data.
-      db = next_descriptor_binary(binary)
-      descriptor_type = db[1].ord
-
-      # Create a ruby object for the descriptor.
-      klass = SubDescriptors[descriptor_type]
-      raise Usb::DescriptorParsingError, "Unrecognized descriptor type: #{descriptor_type}." if klass.nil?
-
-      config.descendents << klass.from_binary(db)
+    def initialize()
+      @descendents = []
+      @children = []
     end
 
-    # Organize the descriptors in a heirarchy.
-    current_ancestors = [config]
-    config.descendents.each do |desc|
-      while !desc.can_be_child_of?(current_ancestors.last)
-        current_ancestors.pop
+    def self.next_descriptor_binary(binary)
+      raise ArgumentError if binary.empty?
+      bLength = binary[0].ord
+      raise Usb::DescriptorParsingError, "Unexpected end of descriptors: bLength=#{bLength} but only #{binary.length} bytes remaining." if binary.length < bLength 
+      descriptor = binary[0, bLength]
+      binary.replace(binary[bLength..-1])
+      return descriptor
+    end
+
+    def self.from_binary(binary)
+      config = new
+      bLength, bDescriptorType, wTotalLength, config.bNumInterfaces, config.bConfigurationValue, config.iConfiguration, config.bmAttributes, config.bMaxPower = binary.unpack('CCvCCCCC')
+
+      raise Usb::DescriptorParsingError, "Expected bLength of configuration descriptor to be 9, but got #{bLength}." if bLength != 9
+
+      binary = binary[9..-1]
+
+      while !binary.empty?
+        # Get the descriptor's binary data.
+        db = next_descriptor_binary(binary)
+        descriptor_type = db[1].ord
+
+        # Create a ruby object for the descriptor.
+        klass = SubDescriptors[descriptor_type]
+        raise Usb::DescriptorParsingError, "Unrecognized descriptor type: #{descriptor_type}." if klass.nil?
+
+        config.descendents << klass.from_binary(db)
       end
-      current_ancestors.last.children << desc
-      current_ancestors << desc
+
+      # Organize the descriptors in a heirarchy.
+      current_ancestors = [config]
+      config.descendents.each do |desc|
+        while !desc.can_be_child_of?(current_ancestors.last)
+          current_ancestors.pop
+        end
+        current_ancestors.last.children << desc
+        current_ancestors << desc
+      end
+
+      config
     end
-
-    config
   end
+
+  class Descriptor
+    def self.descriptor_type(descriptor_type)
+      Configuration::SubDescriptors[4] = Usb::Descriptors::Interface      
+    end
+  end
+
+  class Interface < Descriptor
+    descriptor_type 4
+
+    attr_accessor :bInterfaceNumber
+    attr_accessor :bAlternateSetting
+    attr_accessor :bNumEndpoints
+    attr_accessor :bInterfaceClass
+    attr_accessor :bInterfaceSubClass
+    attr_accessor :bInterfaceProtocol
+    attr_accessor :iInterface
+
+    def self.from_binary(binary)
+      i = new
+      bLength, i.bInterfaceNumber, i.bAlternateSetting, i.bNumEndpoints, i.bInterfaceClass, i.bInterfaceSubClass, i.bInterfaceProtocol, i.iInterface = binary.unpack "CCCCCCCC"
+    end
+  end
+
 end
 
-class Usb::Descriptors::Interface
-  attr_accessor :bInterfaceNumber
-  attr_accessor :bAlternateSetting
-  attr_accessor :bNumEndpoints
-  attr_accessor :bInterfaceClass
-  attr_accessor :bInterfaceSubClass
-  attr_accessor :bInterfaceProtocol
-  attr_accessor :iInterface
 
-  def self.from_binary(binary)
-    i = new
-    bLength, i.bInterfaceNumber, i.bAlternateSetting, i.bNumEndpoints, i.bInterfaceClass, i.bInterfaceSubClass, i.bInterfaceProtocol, i.iInterface = binary.unpack "CCCCCCCC"
-    
-  end
-end
-
-Usb::Descriptors::Configuration::SubDescriptors[4] = Usb::Descriptors::Interface
